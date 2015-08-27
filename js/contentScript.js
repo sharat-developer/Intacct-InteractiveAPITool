77a3e79a2f4425a5f2189d1872bea514b76627b8
Sharat Hegde <sharat.developer@gmail.com>
2015-08-27 12:27:22 +0530

    contentScript.js  enabled in manifest.json

---
+++ js/contentScript.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Sharat Hegde <shegde@intacct.com> on 8/27/2015.
+ */
+
+/**
+ *   contentScript.js file contains scripts which needs to be run in background on each browser page instantiation.
+ *
+ */
+
+//alert("contentScript.js is invoked");
